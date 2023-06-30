3bef695ab13fc6318e8f86809fc913ec99fda255
vitamin777 <rlaalsdn8@gmail.com>
2023-06-30 22:21:14 +0900

    feat: docs (build file) git add

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,9 @@
 
-E
-.storybook/preview.js,6\0\60882ef9544224120d1d72534f44b973f3a57746
-I
-src/components/Button.jsx,c\d\cdcbe0056a02a37eddf7a00a0bf929c431b73e4e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+A
+package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+9
+	yarn.lock,5\3\53585a096fe3a3b96321113856ae62602995a33b
---
+++ docs/Button-stories.332f92ce.iframe.bundle.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkcdd_storybook=self.webpackChunkcdd_storybook||[]).push([[463],{"./src/components/Button.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>Button_stories});__webpack_require__("./node_modules/react/index.js");var prop_types=__webpack_require__("./node_modules/prop-types/index.js"),prop_types_default=__webpack_require__.n(prop_types),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const Button=({color,backgroundColor,label,...props})=>(0,jsx_runtime.jsx)(DefaultButton,{$color:color,$backgroundColor:backgroundColor,...props,children:label});Button.displayName="Button";const DefaultButton=styled_components_browser_esm.ZP.button`
+  color: ${props=>props.$color?props.$color:"#000"}
+  background-color: ${props=>props.$backgroundColor?props.$backgroundColor:"#BF4F74"}
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+`;Button.propTypes={color:prop_types_default().string,backgroundColor:prop_types_default().string,label:prop_types_default().string.isRequired,onClick:prop_types_default().func},Button.defaultProps={color:null,backgroundColor:null,onClick:void 0},Button.__docgenInfo={description:"사용자 상호 작용을 위한 기본 UI 구성 요소",methods:[],displayName:"Button",props:{color:{defaultValue:{value:"null",computed:!1},description:"이것이 페이지의 주요 클릭 유도문안입니까?",type:{name:"string"},required:!1},backgroundColor:{defaultValue:{value:"null",computed:!1},description:"사용할 배경색",type:{name:"string"},required:!1},onClick:{defaultValue:{value:"undefined",computed:!0},description:"선택적 클릭 핸들러",type:{name:"func"},required:!1},label:{description:"버튼 내용",type:{name:"string"},required:!0}}};const Button_stories={title:"Example/Button",component:Button,tags:["autodocs"],argTypes:{backgroundColor:{control:"color"},color:{control:"color"}}},Default={args:{label:"Button"}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {\n    label: 'Button'\n  }\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]}}]);
+//# sourceMappingURL=Button-stories.332f92ce.iframe.bundle.js.map
